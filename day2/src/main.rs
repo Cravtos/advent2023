e35e697b4aa5cf839d18a461d19bd164246951dc
Nikita Kryukov <cravtos92@gmail.com>
2023-12-02 22:30:44 +0700

    day2v2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,33 +1,30 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-const MAX_RED: u32 = 12;
-const MAX_GREEN: u32 = 13;
-const MAX_BLUE: u32 = 14;
-
 fn main() {
     let file = File::open("input.txt").expect("file input.txt should exist");
     let lines = io::BufReader::new(file).lines();
 
     let mut sum = 0;
 
-    'next_line: for (i, line) in lines.enumerate() {
+    for line in lines {
         let line = line.expect("line should be readable");
         let mut iter = line.split_whitespace().skip(2);
         
+        let mut mins = vec![0u32; 3];
         while let Some(amount) = iter.next() {
             let amount = amount.parse::<u32>().expect("should be able to parse a number");
             
             let color = iter.next().expect("color should follow a number");
             match color.chars().nth(0) {
-                Some('r') => if amount > MAX_RED { continue 'next_line; },
-                Some('b') => if amount > MAX_BLUE { continue 'next_line; },
-                Some('g') => if amount > MAX_GREEN { continue 'next_line; },
+                Some('r') => if amount > mins[0] { mins[0] = amount },
+                Some('b') => if amount > mins[1] { mins[1] = amount },
+                Some('g') => if amount > mins[2] { mins[2] = amount },
                 _ => panic!("color should follow a number")
             }
         }
 
-        sum += i + 1;
+        sum += mins[0] * mins[1] * mins[2];
     }
 
     println!("{sum}");
