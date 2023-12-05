3ad6ab495c150984384e3428a87b0abf86eb2eee
Nikita Kryukov <cravtos92@gmail.com>
2023-12-06 01:01:15 +0700

    fmt

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,8 +1,9 @@
+type Range = [u64; 2];
 type MapRange = Vec<u64>;
 type Map = Vec<MapRange>;
 type Almanac = Vec<Map>;
 
-fn intersect(f: &[u64; 2], s: &[u64; 2]) -> Option<[u64; 2]> {
+fn intersect(f: &Range, s: &Range) -> Option<Range> {
     if f[0] > s[1] || s[0] > f[1] {
         return None;
     }
@@ -10,8 +11,8 @@ fn intersect(f: &[u64; 2], s: &[u64; 2]) -> Option<[u64; 2]> {
     Some([f[0].max(s[0]), f[1].min(s[1])])
 }
 
-fn remove_part(full: &[u64; 2], part: &[u64; 2]) -> Vec<[u64; 2]> {
-    let mut parts: Vec<[u64; 2]> = Vec::new();
+fn remove_part(full: &Range, part: &Range) -> Vec<Range> {
+    let mut parts: Vec<Range> = Vec::new();
 
     if full[0] < part[0] {
         parts.push([full[0], part[0] - 1]);
@@ -24,21 +25,22 @@ fn remove_part(full: &[u64; 2], part: &[u64; 2]) -> Vec<[u64; 2]> {
     parts
 }
 
-fn dest(from: &[u64; 2], map: &Map) -> Vec<[u64; 2]> {
-    // check if src range intersects with any range with map and write down intersections
-    let mut destinations: Vec<[u64; 2]> = Vec::new();
-    let mut untouched: Vec<[u64; 2]> = vec![*from];
+fn dest(from: &Range, map: &Map) -> Vec<Range> {
+    let mut destinations: Vec<Range> = Vec::new();
+    let mut untouched: Vec<Range> = vec![*from];
+    
     for range in map {
         let dst = [range[0], range[0] + range[2] - 1];
         let src = [range[1], range[1] + range[2] - 1];
 
+        // check if src range intersects with any range with map and write down intersections
         if let Some(intr) = intersect(from, &src) {
             let intr_dst = [intr[0] - src[0] + dst[0], intr[1] - src[0] + dst[0]];
             destinations.push(intr_dst);
         }
 
         // if range does not intersect, keep it as is
-        let mut new_untouched: Vec<[u64; 2]> = Vec::new();
+        let mut new_untouched: Vec<Range> = Vec::new();
         for unt in untouched.iter() {
             if let Some(intr) = intersect(unt, &src) {
                 let mut untouched_parts = remove_part(unt, &intr);
@@ -81,13 +83,13 @@ fn main() {
     let seeds = parse_seeds(almanac.next().unwrap());
     let almanac: Almanac = almanac.map(parse_map).collect::<Almanac>();
 
-    let mut seeds: Vec<[u64; 2]> = seeds
+    let mut seeds: Vec<Range> = seeds
         .chunks_exact(2)
         .map(|x| [x[0], x[0] + x[1] - 1])
         .collect();
 
     for map in almanac {
-        let mut new_seeds: Vec<[u64; 2]> = Vec::new();
+        let mut new_seeds: Vec<Range> = Vec::new();
         for seed in seeds.iter() {
             new_seeds.append(&mut dest(seed, &map));
         }
