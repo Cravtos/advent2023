b03f6ff2c538e5ffb9478b142a201bd76ef28b50
Nikita Kryukov <cravtos92@gmail.com>
2023-12-06 12:59:58 +0700

    day6v2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,27 +1,11 @@
-fn parse_line(s: &str) -> Vec<u64> {
-    s.split(':')
-        .nth(1)
-        .unwrap()
-        .split_whitespace()
-        .map(|n| n.parse::<u64>())
-        .map(Result::unwrap)
-        .collect()
-}
-
 fn main() {
     let mut input = include_str!("../input.txt").split('\n');
-    let limits = parse_line(input.next().unwrap());
-    let records = parse_line(input.next().unwrap());
+    let limit = input.next().unwrap().split(':').nth(1).unwrap().replace(' ', "").parse::<usize>().unwrap();
+    let record = input.next().unwrap().split(':').nth(1).unwrap().replace(' ', "").parse::<usize>().unwrap();
 
-    let sum: usize = limits
-        .iter()
-        .zip(records)
-        .map(|(&limit, record)| {
-            (1..limit)
+    let sum = (1..limit)
                 .map(|charge_time| (limit - charge_time) * charge_time)
                 .filter(|&distance| distance > record)
-                .count()
-        })
-        .product();
+                .count();
     println!("{sum}");
 }
